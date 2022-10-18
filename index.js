9f568195603aa9c28a262112ff039b33307e4852
Josell Vibar <vibarjosell@gmail.com>
2022-10-18 20:49:27 +0800

    add h1 with the text my first node server

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (request, response) => {
-   console.log(request);
-   response.send('hello world!');
+app.get("/", (req, res) => {
+   // console.log(request);
+   res.send('<h1>My First Node Server!!</h1>');
 });
 
 app.listen('3000', () => {
