fc2eebc5c9cf67fa7c5d81301283a7a1415b180c
Josell Vibar <vibarjosell@gmail.com>
2022-10-18 21:19:20 +0800

    add contact route and install nodemon

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 
 app.get("/", (req, res) => {
    // console.log(request);
-   res.send('<h1>My First Node Server!!</h1>');
+   res.send('<h1>My First Node Server!! HELLO!!!</h1>');
+});
+
+app.get('/contact', (req, res) => {
+      res.send('hi please do contact me: xyz@gmail.com');
 });
 
 app.listen('3000', () => {
