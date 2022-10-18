efbc29e2c0351c9311939daccc94b3bc54b5d48e
Josell Vibar <vibarjosell@gmail.com>
2022-10-18 21:43:56 +0800

    add hobbiesPage route with div h1 and string

--- index.js
+++ index.js
@@ -7,7 +7,23 @@ app.get("/", (req, res) => {
 });
 
 app.get('/contact', (req, res) => {
-      res.send('hi please do contact me: xyz@gmail.com');
+   res.send('hi please do contact me: xyz@gmail.com');
+});
+
+app.get('/aboutPage', (req, res) => {
+   res.send('Josell Vibar: Web Developer');
+});
+
+app.get('/hobbiesPage', (req, res) => {
+   res.send(`
+   <div>
+      <h1>Things I Love Doing</h1>
+      <ol>
+         <li>Reading Manga</li>
+         <li>Playing Valorant/dota</li>
+         <li>study webdev</li>
+      </ol>
+   </div>`);
 });
 
 app.listen('3000', () => {
