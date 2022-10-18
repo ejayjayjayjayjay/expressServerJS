3b1a56ccc76b821fbdbfc39f081ec47ca6b7615b
Josell Vibar <vibarjosell@gmail.com>
2022-10-18 20:36:44 +0800

    add basic app get method to display string hello world in the browser

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.get("/", (request, response) => {
+   console.log(request);
+   response.send('hello world!');
+});
 
 app.listen('3000', () => {
-   console.log("server started on port 3000"); 
+   console.log("server started on port 3000");
 });
 
